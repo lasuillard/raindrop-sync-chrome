71ef12d7d5b6762b6a73d439a03fec183ba1ec39
Yuchan Lee <lasuillard@gmail.com>
2023-10-10 12:50:13 +0000

    test: Update test config
    Several changes made for better testing:
    
    - Splitted unit test config  to prevent duplicate coverage report
    
    - Skip collecting coverage from e2e tests
    
    - Refactored config to reuse common fields

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import { defineConfig } from 'vitest/config';
 import manifest from './src/manifest';
 
-export default defineConfig({
+export const viteConfig = {
 	plugins: [svelte(), crx({ manifest })],
 	resolve: {
 		alias: [
@@ -30,24 +30,7 @@ export default defineConfig({
 		port: 5173,
 		strictPort: true,
 		hmr: { port: 5173 }
-	},
-	test: {
-		alias: [
-			{ find: /^svelte$/, replacement: 'svelte/internal' } // BUG: https://github.com/vitest-dev/vitest/issues/2834
-		],
-		include: ['{src,tests}/**/*.{test,spec}.{js,ts}'],
-		coverage: {
-			all: true,
-			include: ['src/**'],
-			exclude: ['src/**/__mocks__/*'],
-			reporter: ['clover', 'html']
-		},
-		setupFiles: ['tests/setup.ts'],
-		api: {
-			// Publish for * if inside container for forwarding
-			host: process.env.CONTAINER ? '0.0.0.0' : '127.0.0.1',
-			port: 51204,
-			strictPort: true
-		}
 	}
-});
+};
+
+export default defineConfig(viteConfig);
---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vitest/config';
+import { viteConfig } from './vite.config';
+
+const { resolve } = viteConfig;
+
+// NOTE: Separate unit test config because coverage report prints out multiple times
+//       It's likely problem of CRXJS plugin
+//
+//       Similar issue: https://github.com/vitest-dev/vitest/issues/3439
+export default defineConfig({
+	plugins: [svelte()],
+	resolve,
+	test: {
+		alias: [
+			{ find: /^svelte$/, replacement: 'svelte/internal' } // BUG: https://github.com/vitest-dev/vitest/issues/2834
+		],
+		include: ['{src,tests}/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			all: true,
+			include: ['src/**'],
+			exclude: ['src/**/__mocks__/*', 'src/**/*.d.ts'],
+			reporter: ['text', 'clover', 'html']
+		},
+		setupFiles: ['tests/setup.ts'],
+		api: {
+			// Publish for * if inside container for forwarding
+			host: process.env.CONTAINER ? '0.0.0.0' : '127.0.0.1',
+			port: 51204,
+			strictPort: true
+		}
+	}
+});

ddd06dcc6d91116edc59b4278c07b8a111f28b79
Yuchan Lee <lasuillard@gmail.com>
2023-11-03 13:12:22 +0000

    chore: Update `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 export default {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
-		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'
+		'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
+		'./node_modules/flowbite-svelte-icons/**/*.{html,js,svelte,ts}'
 	],
 	theme: {
 		extend: {

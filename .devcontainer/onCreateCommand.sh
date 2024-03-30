5e575a67f2c20ff95f8e1fdfc85da37d6344c4d2
Yuchan Lee <lasuillard@gmail.com>
2024-03-30 06:43:40 +0000

    chore(devcontainer): Simplify devcontainer

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-hooks_dir='./.devcontainer/onCreateCommand'
+curl -sL https://sentry.io/get-cli/ | sh
 
-apt update && apt install -y bash-completion
+sudo apt-get update && sudo apt-get install -y bash-completion
 
 echo '
 if [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
 fi
 ' >> ~/.bashrc
-
-# Run user hook scripts
-if [ -d "${hooks_dir}" ]
-then
-    find "${hooks_dir}" -maxdepth 1 -type f -name '*.sh' -exec "{}" \;
-fi
--- .devcontainer/updateContentCommand.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-hooks_dir='./.devcontainer/updateContentCommand'
-
-# Run user hook scripts
-if [ -d "${hooks_dir}" ]
-then
-    find "${hooks_dir}" -maxdepth 1 -type f -name '*.sh' -exec "{}" \;
-fi

1f57ea674752a61962ce197ff5feedd08c40abe4
Yuchan Lee <lasuillard@gmail.com>
2024-07-16 14:09:28 +0000

    [ci skip] chore(devcontainer): Missing `dotenv` and `google-chrome`

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -4,6 +4,8 @@ curl -sL https://sentry.io/get-cli/ | sh
 
 sudo apt-get update && sudo apt-get install -y bash-completion
 
+pipx install dotenv-cli
+
 echo '
 if [ -f /etc/bash_completion ]; then
     . /etc/bash_completion

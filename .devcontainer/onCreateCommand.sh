af0ef23a67cc9cde78c72c96cecca131a6793f65
Yuchan Lee <lasuillard@gmail.com>
2024-09-21 22:28:50 +0900

    chore(devcontainer): Update `onCreateCommand.sh`
    Signed-off-by: Yuchan Lee <lasuillard@gmail.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -2,12 +2,7 @@
 
 curl -sL https://sentry.io/get-cli/ | sh
 
-sudo apt-get update && sudo apt-get install -y bash-completion
+sudo apt-get update && sudo apt-get install -y \
+    bash-completion
 
 pipx install dotenv-cli
-
-echo '
-if [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-fi
-' >> ~/.bashrc

363f7e9808efec759f35cc047dd66f73cdf6d134
Yuchan Lee <lasuillard@gmail.com>
2024-09-26 14:11:18 +0000

    chore: Simple periodic background task example

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,18 +1,28 @@
+import rd from '~/lib/raindrop';
+
 chrome.runtime.onInstalled.addListener(async (details) => {
 	switch (details.reason) {
 		case 'install':
-			await chrome.alarms.create('demo-default-alarm', {
-				delayInMinutes: 0.1,
-				periodInMinutes: 1
-			});
 			console.debug('Extension installed');
 			break;
 		case 'update':
 			console.debug('Extension updated');
+			await chrome.alarms.clearAll();
 			break;
 	}
+	await chrome.alarms.create('demo-default-alarm', {
+		delayInMinutes: 0.1,
+		periodInMinutes: 1
+	});
 });
 
-chrome.alarms.onAlarm.addListener((alarm) => {
+chrome.alarms.onAlarm.addListener(async (alarm) => {
 	console.debug('Alarm fired:', alarm);
+	const currentUser = await rd.user.getCurrentUser();
+	const lastUpdate = currentUser.data.user.lastUpdate;
+	if (!lastUpdate) {
+		console.warn('No last update found in user data');
+		return;
+	}
+	console.log('lastUpdate', lastUpdate);
 });

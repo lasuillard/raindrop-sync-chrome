267fd3bfc60ea773344dd2c53dbe1a465a1a3e7c
Yuchan Lee <lasuillard@gmail.com>
2024-10-07 11:56:57 +0000

    refactor: Make input + eye pair as component

--- src/components/Config.svelte
+++ src/components/Config.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
-	import { Button, FloatingLabelInput, P } from 'flowbite-svelte';
+	import { Button, P } from 'flowbite-svelte';
 	import { get } from 'svelte/store';
-	import Eye from '~/components/Eye.svelte';
+	import SecretInput from '~/components/SecretInput.svelte';
 	import { putMessage } from '~/lib/messages';
 	import { launchWebAuthFlow as _launchWebAuthFlow } from '~/lib/raindrop/auth';
-	import { accessToken, clientID, clientSecret, refreshToken } from '~/lib/settings';
-
-	let showClientID = false;
-	let showClientSecret = false;
-	let showAccessToken = false;
-	let showRefreshToken = false;
+	import * as settings from '~/lib/settings';
 
 	const launchWebAuthFlow = async () => {
 		try {
 			const result = await _launchWebAuthFlow({
-				clientID: get(clientID),
-				clientSecret: get(clientSecret)
+				clientID: get(settings.clientID),
+				clientSecret: get(settings.clientSecret)
 			});
-			accessToken.set(result.accessToken);
-			refreshToken.set(result.refreshToken);
+			settings.accessToken.set(result.accessToken);
+			settings.refreshToken.set(result.refreshToken);
 			putMessage({ type: 'success', message: 'Successfully authorized app.' });
 		} catch (err) {
 			console.error('Failed to authorize app:', err);
@@ -36,81 +31,11 @@
 	</P>
 	<div class="grid grid-cols-1">
 		<div class="mt-6 grid grid-flow-row grid-cols-1 gap-x-4 gap-y-6">
-			<div class="flex">
-				<FloatingLabelInput
-					data-testid="client-id/input"
-					classDiv="w-full mr-2"
-					id="client-id"
-					type={showClientID ? 'text' : 'password'}
-					required
-					bind:value={$clientID}
-				>
-					Client ID
-				</FloatingLabelInput>
-				<Eye
-					data-testid="client-id/show-button"
-					on:click={() => {
-						showClientID = !showClientID;
-					}}
-					bind:open={showClientID}
-				/>
-			</div>
-			<div class="flex">
-				<FloatingLabelInput
-					data-testid="client-secret/input"
-					classDiv="w-full mr-2"
-					id="client-secret"
-					type={showClientSecret ? 'text' : 'password'}
-					required
-					bind:value={$clientSecret}
-				>
-					Client Secret
-				</FloatingLabelInput>
-				<Eye
-					data-testid="client-secret/show-button"
-					on:click={() => {
-						showClientSecret = !showClientSecret;
-					}}
-					bind:open={showClientSecret}
-				/>
-			</div>
-			<div class="flex">
-				<FloatingLabelInput
-					data-testid="access-token/input"
-					classDiv="w-full mr-2"
-					id="access-token"
-					type={showAccessToken ? 'text' : 'password'}
-					bind:value={$accessToken}
-				>
-					Access Token
-				</FloatingLabelInput>
-				<Eye
-					data-testid="access-token/show-button"
-					on:click={() => {
-						showAccessToken = !showAccessToken;
-					}}
-					bind:open={showAccessToken}
-				/>
-			</div>
-			<div class="flex">
-				<FloatingLabelInput
-					data-testid="refresh-token/input"
-					classDiv="w-full mr-2"
-					id="refresh-token"
-					type={showRefreshToken ? 'text' : 'password'}
-					disabled
-					bind:value={$refreshToken}
-				>
-					Refresh Token
-				</FloatingLabelInput>
-				<Eye
-					data-testid="refresh-token/show-button"
-					on:click={() => {
-						showRefreshToken = !showRefreshToken;
-					}}
-					bind:open={showRefreshToken}
-				/>
-			</div>
+			<SecretInput boundStore={settings.clientID}>Client ID</SecretInput>
+			<SecretInput boundStore={settings.clientSecret}>Client Secret</SecretInput>
+			<SecretInput boundStore={settings.accessToken}>Access Token</SecretInput>
+			<SecretInput boundStore={settings.refreshToken}>Refresh Token</SecretInput>
+			<!-- TODO: Flag to en/disable background sync, sync interval in minutes, toggle to run sync on startup -->
 		</div>
 		<!-- TODO: Debug actions: check token validity, force refresh token, etc. -->
 		<div class="mt-3 flex">
---
+++ src/components/SecretInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { FloatingLabelInput } from 'flowbite-svelte';
+	import { EyeSlashSolid, EyeSolid } from 'flowbite-svelte-icons';
+	import type { Writable } from 'svelte/store';
+
+	export let boundStore: Writable<unknown>;
+	let showSecret = false;
+</script>
+
+<div class="flex">
+	<FloatingLabelInput
+		classDiv="w-full mr-2"
+		type={showSecret ? 'text' : 'password'}
+		required
+		bind:value={$boundStore}
+	>
+		<slot />
+	</FloatingLabelInput>
+
+	<button
+		class="focus:outline-none"
+		type="button"
+		on:click={() => {
+			showSecret = !showSecret;
+		}}
+	>
+		{#if showSecret}
+			<EyeSolid size="sm" />
+		{:else}
+			<EyeSlashSolid size="sm" />
+		{/if}
+	</button>
+</div>
